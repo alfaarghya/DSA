fb8d15c97f1c34629c015ea8d0f17931181d1d36
alfaarghya <alfaarghya.dev@gmail.com>
2025-05-12 10:24:21 +0530

    LC295: Find Median from Data Stream

---
+++ 09-Heap/Heap01_FindMedianfromDataStream.java
@@ -0,0 +1,101 @@
+/*
+LC295: Find Median from Data Stream || https://leetcode.com/problems/find-median-from-data-stream/
+
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+    For example, for arr = [2,3,4], the median is 3.
+    For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+
+Implement the MedianFinder class:
+
+    + MedianFinder() initializes the MedianFinder object.
+    + void addNum(int num) adds the integer num from the data stream to the data structure.
+    + double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+ 
+
+Constraints:
+
+    -105 <= num <= 105
+    There will be at least one element in the data structure before calling findMedian.
+    At most 5 * 104 calls will be made to addNum and findMedian.
+
+
+Follow up:
+
+    If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+    If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+ */
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+  PriorityQueue<Integer> minHeap, maxHeap;
+
+  public MedianFinder() {
+    this.minHeap = new PriorityQueue<>();
+    this.maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+  }
+
+  // TC => O(log n)
+  public void addNum(int num) {
+    // store num in maxHeap
+    this.maxHeap.offer(num);
+
+    // poll peek value from maxHeap and store it on minHeap
+    this.minHeap.offer(maxHeap.poll());
+
+    // check the size and adjust maxHeap
+    if (this.minHeap.size() > this.maxHeap.size()) {
+      this.maxHeap.offer(this.minHeap.poll());
+    }
+  }
+
+  // TC => O(1)
+  public double findMedian() {
+    // odd length
+    if (this.maxHeap.size() > this.minHeap.size()) {
+      return this.maxHeap.peek();
+    }
+
+    // even length
+    return (this.maxHeap.peek() + this.minHeap.peek()) / 2.0d;
+  }
+}
+
+public class Heap01_FindMedianfromDataStream {
+  public static void main(String[] args) {
+    MedianFinder obj = new MedianFinder();
+
+    obj.addNum(1);
+    System.out.println("median -> " +  obj.findMedian());
+    obj.addNum(3);
+    System.out.println("median -> " +  obj.findMedian());
+    obj.addNum(4);
+    System.out.println("median -> " +  obj.findMedian());
+    obj.addNum(4);
+    System.out.println("median -> " +  obj.findMedian());
+    obj.addNum(-1);
+    System.out.println("median -> " +  obj.findMedian());
+    obj.addNum(-6);
+    System.out.println("median -> " +  obj.findMedian());
+  }
+}
